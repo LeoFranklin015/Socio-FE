b3c7a2c9ffb9912c8f80de6010fdb17486e987ae
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-05 19:42:06 +0530

    Like Functionality integrated and image fol public

--- src/api/PostAPI.js
+++ src/api/PostAPI.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:4000" });
 
 export const getTimelinePost = (id) => API.post(`/post/getAllPost/${id}`);
+export const likePost = (id, userId) =>
+  API.patch(`/post/like/${id}`, { currentUserId: userId });
