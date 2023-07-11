dd7d69b4e43cae52f1a57adc571824bb2a1e5b99
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-11 13:22:51 +0530

    Followers card fetched form cloud

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,8 +4,8 @@ import { followUser, unfollowUser } from "../../actions/UserAction";
 const User = ({ person }) => {
   const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useSelector((state) => state.authReducer.authData);
-  const dispatch = useDispatch()
-  
+  const dispatch = useDispatch();
+
   const [following, setFollowing] = useState(
     person.followers.includes(user._id)
   );
@@ -21,7 +21,7 @@ const User = ({ person }) => {
         <img
           src={
             publicFolder + person.profilePicture
-              ? publicFolder + person.profilePicture
+              ? person.profilePicture
               : publicFolder + "defaultProfile.png"
           }
           alt="profile"
